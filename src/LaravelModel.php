0cd86eb16446152073990eb52e297f12ac94f2c9
Kurnia Saputra <krnsptr@gmail.com>
2021-11-23 14:10:15 +0700

    feat: Add smartmodel trait to model

--- src/LaravelModel.php
+++ src/LaravelModel.php
@@ -3,6 +3,7 @@
 namespace Krnsptr\LaravelModel;
 
 use Based\Fluent\Fluent;
+use Deiucanta\Smart\SmartModel;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Validator as ValidatorFacade;
 use Illuminate\Validation\Validator;
@@ -10,12 +11,20 @@ use Illuminate\Validation\Validator;
 class LaravelModel extends Model
 {
     use Fluent;
+    use SmartModel;
 
     public ?string $modelLabel;
 
     protected array $fieldLabels = [
         'id' => 'ID',
     ];
+
+    public function __construct(array $attributes = [])
+    {
+        $this->initSmartFields();
+
+        parent::__construct($attributes);
+    }
 
     protected function getData(): array
     {
