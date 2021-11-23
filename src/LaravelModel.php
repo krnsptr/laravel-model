30efa660e3726fd5c46e47c6afa28d56ee60cd8d
Kurnia Saputra <krnsptr@gmail.com>
2021-11-23 14:06:12 +0700

    feat: Add validation to model

--- src/LaravelModel.php
+++ src/LaravelModel.php
@@ -3,8 +3,41 @@
 namespace Krnsptr\LaravelModel;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator as ValidatorFacade;
+use Illuminate\Validation\Validator;
 
 class LaravelModel extends Model
 {
-    // Build your next great package.
+    public ?string $modelLabel;
+
+    protected array $fieldLabels = [
+        'id' => 'ID',
+    ];
+
+    protected function getData(): array
+    {
+        return $this->getAttributes();
+    }
+
+    protected function getRules(): array
+    {
+        return [];
+    }
+
+    public function getValidator(): Validator
+    {
+        return ValidatorFacade::make(
+            $this->getData(),
+            $this->getRules(),
+            [],
+            $this->fieldLabels,
+        );
+    }
+
+    public function save(array $options = []): bool
+    {
+        $this->getValidator()->validate();
+
+        return parent::save($options);
+    }
 }
